f2543e6bce2cb62d85c02c123af59123239a95a0
Alexandre <alexandre.marcos@zup.com.br>
2021-07-26 16:16:39 -0300

    Inicializando o projeto

---
+++ src/test/java/br/com/orangetalents/cadadocodigo/CadaDoCodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.orangetalents.cadadocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadaDoCodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
